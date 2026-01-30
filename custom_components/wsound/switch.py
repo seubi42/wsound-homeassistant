268720690dda52e3b914e44a7e12f905797b421f
Sebastien Mallet <sebastien.mallet@smbb-logiciel.com>
2026-01-30 23:31:00 +0100

    fix switch led

--- custom_components/wsound/switch.py
+++ custom_components/wsound/switch.py
@@ -5,20 +5,29 @@ from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import DOMAIN
 from .coordinator import WSoundCoordinator
+from .helpers import device_info
 
 
 async def async_setup_entry(hass, entry, async_add_entities) -> None:
     coordinator: WSoundCoordinator = hass.data[DOMAIN][entry.entry_id]["coordinator"]
-    async_add_entities([WSoundLedSwitch(coordinator, entry.entry_id)])
+    async_add_entities([WSoundLedSwitch(hass, entry.entry_id, coordinator)])
 
 
 class WSoundLedSwitch(CoordinatorEntity[WSoundCoordinator], SwitchEntity):
     _attr_name = "LED"
     _attr_has_entity_name = True
+    _attr_icon = "mdi:led-on"
 
-    def __init__(self, coordinator: WSoundCoordinator, entry_id: str) -> None:
+    def __init__(self, hass, entry_id: str, coordinator: WSoundCoordinator) -> None:
         super().__init__(coordinator)
+        self._hass = hass
+        self._entry_id = entry_id
         self._attr_unique_id = f"{entry_id}_led"
+
+    @property
+    def device_info(self):
+        host = self._hass.data[DOMAIN][self._entry_id].get("host")
+        return device_info(self._entry_id, host)
 
     @property
     def is_on(self) -> bool:
